87ca656dd91abfc6be85a7d5a8add864b7ca389a
6uzmen <greolon1@gmail.com>
2022-01-14 09:23:13 -0300

    remove bootstrap cdn

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -60,13 +60,9 @@ export default function Head() {
           content="https://zircon.tech/wp-content/uploads/2019/10/Zirconfavicon2.png"
         />
         <meta name="description" content="A trusted partner for the technologies driving the future. We build solutions for your development projects as a nearshore software development company." />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
+
         <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7707659.js"></script>
       </NextHead>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossOrigin="anonymous" />
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, lightTheme } from "../style/theme.config";
 import "../style/globals.css";
-import "../styles/custom.scss";
+import "../style/custom.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
   return (
