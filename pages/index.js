34e0a7b2dddc043d0f8dd4a65b8f366e83efe18e
6uzmen <greolon1@gmail.com>
2022-01-14 10:12:33 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,18 @@ import Head from "../components/Head";
 import Navbar from "../components/Navbar";
 // import AboutUs from "../containers/AboutUs";
 import Blog from "../containers/Blog";
-import ClientsAndTestimonials from "../containers/ClientsAndTestimonials";
+// import ClientsAndTestimonials from "../containers/ClientsAndTestimonials";
 import ContactUs from "../containers/ContactUs";
 import Hero from "../containers/Hero";
 import OurServices from "../containers/OurServices";
 // import CaseStudy from "../containers/CaseStudy";
 // const OurServices = dynamic(() => import("../containers/OurServices"));
+const AboutUs = dynamic(() => import("../containers/AboutUs"));
 const CaseStudy = dynamic(() => import("../containers/CaseStudy"));
 const ClientsAndTestimonials = dynamic(() => import("../containers/ClientsAndTestimonials"));
 // const Blog = dynamic(() => import("../containers/Blog"), { ssr: false, });
 // const ContactUs = dynamic(() => import("../containers/ContactUs"), { ssr: false, });
 // const Navbar = dynamic(() => import("../components/Navbar"), { ssr: false, });
-const AboutUs = dynamic(() => import("../containers/AboutUs"));
 // const Hero = dynamic(() => import("../containers/Hero"), { ssr: false, });
 
 const Home = () => {
