927b10c4c1065a3e63e4956210a6d4cddb1aea16
6uzmen <greolon1@gmail.com>
2022-01-14 09:04:56 -0300

    update styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, lightTheme } from "../style/theme.config";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
