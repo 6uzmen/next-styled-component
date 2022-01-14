3f5651903384cc311880b81d439b922534c159a5
6uzmen <greolon1@gmail.com>
2022-01-14 10:10:50 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import dynamic from "next/dynamic";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Head from "../components/Head";
 import Navbar from "../components/Navbar";
-import AboutUs from "../containers/AboutUs";
+// import AboutUs from "../containers/AboutUs";
 import Blog from "../containers/Blog";
 import ClientsAndTestimonials from "../containers/ClientsAndTestimonials";
 import ContactUs from "../containers/ContactUs";
 import Hero from "../containers/Hero";
 import OurServices from "../containers/OurServices";
-import CaseStudy from "../containers/CaseStudy";
-// const OurServices = dynamic(() => import("../containers/OurServices"), { ssr: false, });
-// const CaseStudy = dynamic(() => import("../containers/CaseStudy"), { ssr: false, });
-// const ClientsAndTestimonials = dynamic(() => import("../containers/ClientsAndTestimonials"), { ssr: false, });
+// import CaseStudy from "../containers/CaseStudy";
+// const OurServices = dynamic(() => import("../containers/OurServices"));
+const CaseStudy = dynamic(() => import("../containers/CaseStudy"));
+const ClientsAndTestimonials = dynamic(() => import("../containers/ClientsAndTestimonials"));
 // const Blog = dynamic(() => import("../containers/Blog"), { ssr: false, });
 // const ContactUs = dynamic(() => import("../containers/ContactUs"), { ssr: false, });
 // const Navbar = dynamic(() => import("../components/Navbar"), { ssr: false, });
-// const AboutUs = dynamic(() => import("../containers/AboutUs"), { ssr: false, });
+const AboutUs = dynamic(() => import("../containers/AboutUs"));
 // const Hero = dynamic(() => import("../containers/Hero"), { ssr: false, });
 
 const Home = () => {
