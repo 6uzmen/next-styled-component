27ccc566033a166948f18075d134d925473c05b5
system32uwu <mateocarriqui7@gmail.com>
2022-02-16 14:13:15 -0300

    Add more missing redirects, fixed broken link

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -52,10 +52,10 @@ export default function Navbar({ variant = false }: IProps) {
       label: "Our Services",
       link: "/#services",
       subItems: [
-        { label: "Blockchain", link: "https://zircon.tech/blockchain-software-company" },
-        { label: "IOT", link: "https://zircon.tech/info/zircontech-services-iot" },
-        { label: "Artificial Intelligence", link: "https://zircon.tech/info/zircontech-services-ai" },
-        { label: "Cloud", link: "https://zircon.tech/info/zircontech-services-cloud-consultancy" },
+        { label: "Blockchain", link: "/info/zircontech-services-blockchain" },
+        { label: "IOT", link: "/info/zircontech-services-iot" },
+        { label: "Artificial Intelligence", link: "/info/zircontech-services-ai" },
+        { label: "Cloud", link: "/info/zircontech-services-cloud-consultancy" },
       ],
     },
     { label: "About Us", link: "/#about-us" },
@@ -73,7 +73,7 @@ export default function Navbar({ variant = false }: IProps) {
         { label: "Monax", link: "/case-study/monax" },
       ],
     },
-    { label: "Blog", link: "https://zircon.tech/blogs" },
+    { label: "Blog", link: "/blogs" },
   ];
   const [scrollPosition, setScrollPosition] = useState(0);
   const [showNavbar, setShowNavbar] = useState(true);
