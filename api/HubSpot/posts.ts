9eee04eded2c79f4861f19ba19dba1b230c75856
system32uwu <mateocarriqui7@gmail.com>
2022-02-26 01:06:43 -0300

    Use temporary backend for blog posts

--- api/HubSpot/posts.ts
+++ api/HubSpot/posts.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosResponse } from "axios";
-import { unAuthAxiosCall } from "../axiosCall";
+import axios from "axios";
 
 export const getBlogPosts = async () =>
   await axios.get(
-    `https://zircontech-backend-gnkum.ondigitalocean.app/api/hubspot/blogs`
+    `https://zircontech-backend-personal.vercel.app/api/blogs/posts`
   );
